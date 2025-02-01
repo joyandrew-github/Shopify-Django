aabd0cbd80e0d130cdad8f479dfd3953ebfb291c
joyandrew <joyandrew006@gmail.com>
2025-02-01 20:03:07 +0530

    save

--- my_project/wsgi.py
+++ my_project/wsgi.py
@@ -1,18 +1,13 @@
-"""
-WSGI config for my_project project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
-"""
-
 import os
-
 from django.core.wsgi import get_wsgi_application
+from waitress import serve
 
-# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'my_project.settings')
+# Set the default Django settings module for the 'application' object.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'shopkart.settings')
 
-
+# Create the WSGI application.
 application = get_wsgi_application()
+
+# For development or local server running, use waitress to serve the app.
+if __name__ == "__main__":
+    serve(application, host='0.0.0.0', port=8080)
