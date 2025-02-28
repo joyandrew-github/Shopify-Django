e00cae836d0964b3c53e1fea4b803e1cb23a25bd
joyandrew <joyandrew006@gmail.com>
2025-02-28 10:46:58 +0530

    Added WhiteNoise for static file handling

--- my_project/settings.py
+++ my_project/settings.py
@@ -1,10 +1,14 @@
 from pathlib import Path
-BASE_DIR = Path(__file__).resolve().parent.parent
 import os
-SECRET_KEY = 'django-insecure-q!o+)bjpda&n^dkv^p62uja#ggwcsuog@x#t7-m0be$!-&#txk'
 
-DEBUG = True
-ALLOWED_HOSTS = []
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', 'django-insecure-q!o+)bjpda&n^dkv^p62uja#ggwcsuog@x#t7-m0be$!-&#txk')
+
+DEBUG = os.getenv('DJANGO_DEBUG', 'True') == 'True'
+ALLOWED_HOSTS = os.getenv('DJANGO_ALLOWED_HOSTS', '127.0.0.1,localhost').split(',')
+
+
 INSTALLED_APPS = [
     'jazzmin',
     'django.contrib.admin',
@@ -19,6 +23,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -50,11 +55,14 @@ WSGI_APPLICATION = 'my_project.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'django_joy',
-        'USER': 'root',
-        'PASSWORD': 'joy123',
-        'HOST': 'localhost',
-        'PORT': '3306',
+        'NAME': os.getenv('DB_NAME', 'django_joy'),
+        'USER': os.getenv('DB_USER', 'root'),
+        'PASSWORD': os.getenv('DB_PASSWORD', 'joy123'),
+        'HOST': os.getenv('DB_HOST', 'localhost'),
+        'PORT': os.getenv('DB_PORT', '3306'),
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
+        }
     }
 }
 
@@ -77,11 +85,13 @@ LANGUAGE_CODE = 'en-us'
 TIME_ZONE = 'UTC'
 USE_I18N = True
 USE_TZ = True
-# STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR / 'static']
-# STATIC_ROOT = BASE_DIR / 'staticfiles'
+
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [BASE_DIR / 'static']
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 MEDIA_URL = '/images/'
 MEDIA_ROOT = BASE_DIR / 'static'
+
+
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
